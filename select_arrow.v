eeff3d0aa230af408c977883514e96c918bc451a
Shirley <shirley.huang2000@outlook.com>
2019-11-18 19:26:37 -0500

    fixed output typo in arrow_select and added clock

--- select_arrow.v
+++ select_arrow.v
@@ -5,14 +5,15 @@
 // 111: NOTHING
 
 // Selects appropriate arrow based on key inputs 
-module select_arrow(left, right, up, down, direction_out);
-	input left, right, up, down;
-	ouput direction_out; 
+module select_arrow(clock, left, right, up, down, direction_out);
+	// clock to continuously update 
+	input left, right, up, down, clock;
+	output direction_out; 
 	
 	localparam direction = {left, right, up, down};
-	always @(*) 
+	always @(posedge clock) 
 		case (direction): 
-			4'b0001: direction_out <= 3'b001; //If down is pressed, arrow is down
+			4'b0001: direction_out <= 3'b001; // If down is pressed, arrow is down
 			4'b0010: direction_out <= 3'b010; // If up is pressed, arrow is up
 			4'b0100: direction_out <= 3'b011; // If right is pressed, arrow is right
 			4'b1000: direction_out <= 3'b000; // If left is pressed, arrow is left
