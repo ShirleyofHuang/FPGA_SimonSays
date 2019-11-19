decc49ae921b0b50e0d9c0e321d23b85f7587d4d
Alexander Xu Chen <alexxu.chen@mail.utoronto.ca>
2019-11-19 11:40:55 -0500

    changed datapath to include x and y input and removed loading

--- datapath.v
+++ datapath.v
@@ -1,44 +1,23 @@
 module datapath(
 					clk, 
 					resetn, 
-					ld_x, 
-					ld_y, 
-					ld_colour, 
-					colour_in, 
-					coordinate, 
+					x_in,
+					y_in
+					colour_in,  
 					x_out, 
 					y_out, 
 					colour_out
 					)
-	input clk, resetn, ld_x, ld_y, ld_colour,
-	input [2:0] colour_in,
-	input [6:0] coordinate, 
-	output [7:0] x_out, 
-	output [6:0] y_out, 
-	output [2:0] colour_out
-	);
+	input clk, resetn;
+	input [7:0] x_in;
+	input [6:0] y_in;
+	input [2:0] colour_in; 
+	output [7:0] x_out;
+	output [6:0] y_out;
+	output [2:0] colour_out;
 
-	reg [7:0] x;
-	reg [6:0] y;
-	reg [2:0] colour;
 	reg [3:0] counter;
 
-	//reset or load
-	always @(posedge clk) begin
-		if (!resetn) begin
-			x <= 8'b0;
-			y <= 7'b0;
-			colour <= 3'b0;
-		end
-		else begin
-			if (ld_x)
-				x <= {1'b0, coordinate};
-			if (ld_y)
-				y <= coordinate;
-			if (ld_colour)
-				colour <= colour_in;
-		end
-	end
 	//counter
 	always @(posedge clk) begin
 		if (!resetn)
@@ -50,8 +29,8 @@ module datapath(
 				counter <= counter + 1'b1;
 	end
 
-	assign x_out = x + counter[1:0];
-	assign y_out = y + counter[3:2];
+	assign x_out = x_in + counter[1:0];
+	assign y_out = y_in + counter[3:2];
 	assign colour_out = colour;
 	
 endmodule
