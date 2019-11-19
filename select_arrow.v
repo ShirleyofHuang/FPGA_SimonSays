1c82974a3061f7bd12a77e5da93965d4eb09d3ae
Alexander Xu Chen <alexxu.chen@mail.utoronto.ca>
2019-11-19 11:41:28 -0500

    improved consistency of arrow bits

--- select_arrow.v
+++ select_arrow.v
@@ -1,23 +1,30 @@
-// 001: DOWN
-// 010: UP
-// 011: RIGHT
-// 000: LEFT
+// 000: UP
+// 001: RIGHT
+// 010: DOWN
+// 011: LEFT
 // 111: NOTHING
 
 // Selects appropriate arrow based on key inputs 
-module select_arrow(clock, left, right, up, down, direction_out);
+module select_arrow(
+						clock, 
+						up, 
+						right, 
+						up, 
+						down, 
+						left, 
+						direction_out
+						);
 	// clock to continuously update 
-	input left, right, up, down, clock;
+	input up, right, down, left, clock;
 	output direction_out; 
 	
-	localparam direction = {left, right, up, down};
+	localparam direction = {up, right, down, left};
 	always @(posedge clock) 
 		case (direction): 
-			4'b0001: direction_out <= 3'b001; // If down is pressed, arrow is down
-			4'b0010: direction_out <= 3'b010; // If up is pressed, arrow is up
-			4'b0100: direction_out <= 3'b011; // If right is pressed, arrow is right
-			4'b1000: direction_out <= 3'b000; // If left is pressed, arrow is left
-			default: direction_out <= 3'b111;  // else, we do nothing 
-		
+			4'b1000: direction_out <= 3'b000; // If up is pressed, arrow is up
+			4'b0100: direction_out <= 3'b001; // If right is pressed, arrow is right
+			4'b0010: direction_out <= 3'b010; // If down is pressed, arrow is down
+			4'b0001: direction_out <= 3'b011; // If left is pressed, arrow is left
+			default: direction_out <= 3'b111; // else, we do nothing 
 		endcase 
 endmodule 
