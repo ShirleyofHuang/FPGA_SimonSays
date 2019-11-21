9f8a455b493975a8d814aa7ba234fa0880813fc6
FuzPeachFuz <52029551+FuzPeachFuz@users.noreply.github.com>
2019-11-20 20:58:32 -0500

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -3,12 +3,13 @@ module datapath(
 					resetn, 
 					x_in,
 					y_in,
+					clicked,
 					colour_in,  
 					x_out, 
 					y_out, 
 					colour_out
 					);
-	input clk, resetn;
+	input clk, resetn, clicked;
 	input [7:0] x_in;
 	input [6:0] y_in;
 	input [2:0] colour_in; 
@@ -17,22 +18,29 @@ module datapath(
 	output [2:0] colour_out;
 
 	reg [3:0] counter;
+	reg [2:0]actual_color;
 
 	//counter
 	always @(posedge clk) begin
-		if (!resetn)
-			counter <= 4'b0000;
-		else
-			if (counter == 1111)
+//		if (clicked) begin
+			if (!resetn)
 				counter <= 4'b0000;
 			else
+					if (counter == 1111)
+						counter <= 4'b0000;
+					else
 				counter <= counter + 1'b1;
-	end
+		end
+//		else begin
+//			counter <= 4'b0000;
+//			actual_color <= 3'b111;
+//			end
 
 	assign x_out = x_in;
 	assign y_out = y_in;
 //	assign x_out = x_in + counter[1:0];
 //	assign y_out = y_in + counter[3:2];
+//	assign colour_out = actual_color;
 	assign colour_out = colour_in;
 	
 endmodule
